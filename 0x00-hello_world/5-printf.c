05e6c1a8023bd211d0815892f231e8c68a45a9c1
Abdulmalik Lawal <lawalabdulmalik59@gmail.com>
2023-10-05 11:41:37 -0700

    me myself

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * using the print command an dnot put
+ * Ends with Return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return(0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    // Variable declarations
+    int a;
+    long int b;
+    long long int c;
+    char d;
+    float f;
+
+    // Printing the sizes of the data types
+    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+    // Return 0 to indicate successful execution
+    return (0);
+}
+
