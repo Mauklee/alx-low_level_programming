f135992e64b4c8d1a9213a0c7cd176966aa52408
Abdulmalik Lawal <lawalabdulmalik59@gmail.com>
2023-10-06 18:46:29 -0700

    Task 7 completed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main-Entry Point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	/*int j;*/
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	
+	putchar('\n');
+	/**
+	*for (j = 65; j <= 90; j++)
+	*{	putchar(j);
+	*}
+	*/
+	return (0);
+
+}
