9a288b7ff0ba5766342099ffc1ddf0a638281a9a
Abdulmalik Lawal <lawalabdulmalik59@gmail.com>
2023-10-06 17:11:33 -0700

    task 1-3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - This is entry point
+*
+* Return (0) means sucessful
+*/
+int main(void) 
+{
+	int i;
+
+	for  (i = 97; i <= 122; i++ )
+	{
+		putchar(i);
+	}
+	putchar('n')
+
+	return(0);
+}
