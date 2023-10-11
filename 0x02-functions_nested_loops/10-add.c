81292da5addf9571751537bdfb7d9ac62128bded
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-11 05:01:19 +0100

    Number 9 n 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Prints the times table of 9
+ *
+ * Return: nothing
+ * @a : the variabe
+ * @b  : second variable
+ *
+ */
+	int add(int a, int b)
+{
+	int results;
+	results = a + b;
+
+	return (results);
+}
