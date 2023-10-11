76c53efb54f7227fbb76caea450db5d848e037e9
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-11 02:11:17 +0100

    Alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * 
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+    int i, j;
+    for (j =0; j < 10; j++)
+    { 
+        for (i = 97; i <= 122; i++)
+	{
+		    putchar(i);
+	}
+        putchar('\n');
+    }
+
+}
