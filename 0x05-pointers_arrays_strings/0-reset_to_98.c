86c360642f14b9d54a2bd6632cf967ca8dae8db1
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-17 07:11:38 +0100

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/**
+ * reset_to_98 - Updates the value of reference to 98
+ *
+ * @n: Pointer
+ *
+ * Return: 0 as success
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Beginning of ointers file
+
