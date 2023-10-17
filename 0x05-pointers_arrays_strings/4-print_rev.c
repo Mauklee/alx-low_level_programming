2ae4c50eb165913f0df9b0dbbc32ebe450d30c64
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-17 07:54:06 +0100

    Fifth  task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ *
+ * @s: the string
+ *
+ * Return: void
+ *
+ */
+
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n');
+}
