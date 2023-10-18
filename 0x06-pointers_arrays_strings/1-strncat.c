455991f7c77b3c8b960362b3e014dae1bf86fc28
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-19 00:17:28 +0100

    Second task using strn

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _strncat - _strchat appends the src string to the dest string
+* @dest: destination pointer
+* @src: source string
+* @n: the value
+* Return:;wq dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_ptr = dest;
+
+	while (*dest_ptr != '\0')
+	{
+		dest_ptr++;
+	}
+	while (*src != '\0' && n > 0)
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++;
+		n--;
+	}
+	*dest_ptr = '\0';
+
+	return (dest);
+
+}
