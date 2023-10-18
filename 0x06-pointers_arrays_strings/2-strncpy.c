0ef0ba97b20e3c91ae4a3f0f38be48228b36aa51
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-19 00:26:54 +0100

    Third task using strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _strncpy - _strcpy copies thes tring
+* @dest: destination pointer
+* @src: source string
+* @n: the value
+* Return:;wq dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_ptr = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++;
+		n--;
+	}
+
+	return (dest);
+
+}
