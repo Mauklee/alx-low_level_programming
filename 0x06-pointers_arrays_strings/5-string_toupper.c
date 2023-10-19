d851d0edce31d78cfdbcd33ccbc974ca03a1ed92
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-19 22:34:08 +0100

    Fiftth  task using adjusted  strncmp

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - conerts the string to uppercase
+ *
+ * @str: string to converted
+ *
+ * Return: uppercase letters
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if  (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+
+		ptr++;
+	}
+	return (str);
+}
