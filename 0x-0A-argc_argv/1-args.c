db7ce32917d653498030285945268950a3a95d80
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-10-28 05:33:07 +0100

    Argc Arg V  task

---
+++ 0x-0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments
+ *
+ * @argc: argument count
+ * @argv: an array of the argumentsi
+ *
+ * Return: 0 for success
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+
+}
