3e31980c70a21091637f915189f4403fa7bd8bc0
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-11-07 10:27:54 +0100

    redo preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main- Print name of file
+ * Return: gives the file name
+*/
 
-int main(void) {
-    printf("%s\n", __FILE__);
-    return 0;
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
