fa68e903f001c9e3496f3831c05b7c33387ecad2
Abdulmalik <lawalabdulmalik59@gmail.com>
2023-11-09 00:51:02 +0100

    first and second task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#define NULL 0
+
 
 /**
  * print_name - Prints the name given
@@ -12,7 +12,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name != NULL || f != NULL)
 		(*f)(name);
 	else
 		return;
